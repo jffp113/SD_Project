5149f222bfc54afe773979fc873a5db11bcf6105
Pedro Camponês <p.campones@campus.fct.unl.pt>
2019-04-09 08:27:40 +0100

    nitpicks

--- SD2019-Microgram-Code/src/microgram/impl/clt/java/RetryPostsClient.java
+++ SD2019-Microgram-Code/src/microgram/impl/clt/java/RetryPostsClient.java
@@ -9,7 +9,7 @@ import microgram.api.java.Result;
 //We implemented TODO
 public class RetryPostsClient extends RetryClient implements Posts {
 
-	final Posts impl;
+	private final Posts impl;
 	
 	public RetryPostsClient( Posts impl ) {
 		this.impl = impl;
@@ -17,41 +17,41 @@ public class RetryPostsClient extends RetryClient implements Posts {
 
 	@Override
 	public Result<Post> getPost(String postId) {
-		return reTry( () -> impl.getPost(postId));
+		return super.reTry( () -> impl.getPost(postId));
 	}
 
 	@Override
 	public Result<String> createPost(Post post) {
-		return reTry(() -> impl.createPost(post));
+		return super.reTry(() -> impl.createPost(post));
 	}
 
 	@Override
 	public Result<Void> deletePost(String postId) {
-		return reTry(() -> impl.deletePost(postId));
+		return super.reTry(() -> impl.deletePost(postId));
 	}
 
 	@Override
 	public Result<Void> like(String postId, String userId, boolean isLiked) {
-		return reTry(() -> impl.like(postId, userId, isLiked));
+		return super.reTry(() -> impl.like(postId, userId, isLiked));
 	}
 
 	@Override
 	public Result<Boolean> isLiked(String postId, String userId) {
-		return reTry(() -> impl.isLiked(postId, userId));
+		return super.reTry(() -> impl.isLiked(postId, userId));
 	}
 
 	@Override
 	public Result<List<String>> getPosts(String userId) {
-		return reTry(() -> impl.getPosts(userId));
+		return super.reTry(() -> impl.getPosts(userId));
 	}
 
 	@Override
 	public Result<List<String>> getFeed(String userId) {
-		return reTry(() -> impl.getFeed(userId));
+		return super.reTry(() -> impl.getFeed(userId));
 	}
 
 	@Override
 	public Result<Void> removeAllPostsFromUser(String userId) {
-		return reTry(() -> impl.removeAllPostsFromUser(userId));
+		return super.reTry(() -> impl.removeAllPostsFromUser(userId));
 	}
 }
